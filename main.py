7cf76c00eb55d5e21e94ebcccd76884e421854ee
terenceithaque <terence.lethierry@protonmail.com>
2023-10-29 12:47:25 +0100

    Ajout classe Application

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Programme pour convertir l'audio d'une vidéo en un fichier texte
+from tkinter import *
+import moviepy.editor as mp
+import speech_recognition as sr
+
+
+class Application(Tk):
+    "Application"
+
+    def __init__(self):
+        "Constructeur de l'application"
+        super().__init__()
